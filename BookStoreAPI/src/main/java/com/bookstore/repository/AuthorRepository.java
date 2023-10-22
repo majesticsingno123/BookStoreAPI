c941b022aa9de07a18f79ef78da05e3da72d3f4a
majesticsingno123 <54814422+majesticsingno123@users.noreply.github.com>
2023-10-22 22:44:25 +0800

    Delete BookStoreAPI directory

--- BookStoreAPI/src/main/java/com/bookstore/repository/AuthorRepository.java
+++
@@ -1,7 +0,0 @@
-package com.bookstore.repository;
-
-import com.bookstore.entity.Author;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AuthorRepository extends JpaRepository<Author, Long> {
-}
